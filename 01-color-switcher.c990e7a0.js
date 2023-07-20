c0c62cf570d507e0ac63424c17e639e2ebc87398
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-07-20 22:01:56 +0000

    Deploying to gh-pages from @ Vitalii-Malyk/goit-js-hw-09@649dc018aa7daee3333248981635723c3cc0116a 🚀

---
+++ 01-color-switcher.c990e7a0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");let r=null;e.addEventListener("click",(()=>{r=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0}),1e3)})),t.addEventListener("click",(()=>{clearInterval(r),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.c990e7a0.js.map
